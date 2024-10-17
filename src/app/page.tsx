83b7f3d2374850177e7202915acf7f46f2f2d43b
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-10-17 15:40:06 +0900

    [add]cssでボタン等の整理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TodoApp from "./TodoApp";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.title}>Todo App</h1>
+      <h1 className={styles.page_title}>Todo App</h1>
       <TodoApp/>
     </div>
   );
