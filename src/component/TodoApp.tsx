1db9d7617aca6fc82b086d5d2fe88d90ad7affdb
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-10-18 15:40:49 +0900

    [wip]ローカルのリスト更新について

---
+++ src/component/TodoApp.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import styles from "./todo.module.css"
+import TodoList from "./TodoList";
+import { addTodo, getAllTodos } from "@/utils/supabese_functions";
+import { Todo } from "@/utils/interface";
+
+const TodoApp = () => {
+
+ 
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState<string>("");
+    
+    useEffect(() => {
+      const getTodos = async () => {
+      const todos = await getAllTodos();
+        setTodos(todos || []);
+        console.log(todos);
+      }
+      getTodos();
+    }, []);
+
+    const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (title === "") return
+
+      await addTodo(title);
+      setTodos((prevTodos) =>{
+        const newTodos = [...prevTodos,{id:1 ,title:title,isCompleted:false}]
+        return newTodos;
+      });
+
+      setTitle("");
+  };
+
+    return (
+      <div>
+        <form className={styles.todo_form} onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" placeholder="やること…"
+          className={styles.input}
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}/>
+          <button className={styles.add_btn}>記入</button>
+        </form>
+        <TodoList todos={todos} setTodos={setTodos}/>
+      </div>
+    );
+  }
+
+  export default TodoApp
